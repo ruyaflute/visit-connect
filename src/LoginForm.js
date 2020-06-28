746a7b0eb9931cc8cfb20171ce4403f2f92420f3
nancyruya <nancyruya@gmail.com>
2020-06-28 15:55:39 -0400

    login

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import InputField from './InputField';
+import SumbitButton from './SubmitButton';
+import UserStore from './stores/UserStore';
 
 class LoginForm extends React.Component {
   render() {
   return (
     <div className="loginForm">
-      jhlhlkjl
+      Login form
     </div>
   );
 }
