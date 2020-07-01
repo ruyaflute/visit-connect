807b3fed539702adf45408aaf8165b43bf3d3129
nancyruya <nancyruya@gmail.com>
2020-06-30 20:39:12 -0400

    style

--- src/Patient.js
+++ src/Patient.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
+import { Col, Row, Form, InputGroup } from "react-bootstrap";
 import DatePicker from "react-datepicker";
-import { Button, FormControl } from 'react-bootstrap';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup'
+import { Button, FormControl, Container } from 'react-bootstrap';
+import './App.css';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -30,7 +28,7 @@ class Patient extends React.Component {
         <Form.Control type="range" />
         </Form.Group>
 
-        <h5> I'm requesting an visit for: </h5>
+        <h5 className="visitFor"> I'm requesting an visit for: </h5>
         <DatePicker
         selected={this.state.startDate}
         onChange={this.handleChange}
@@ -49,14 +47,44 @@ class Patient extends React.Component {
   </Form.Group>
 
   <Row>
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City: </Form.Label>
-      <Form.Control />
+
+    <Form.Group  as={Row}>
+      <Form.Label as="legend" column sm={2}>
+        City: 
+      </Form.Label>
+      <Col sm={10}>
+        <Form.Check
+          type="radio"
+          label="Manhattan"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios1"
+        />
+        <Form.Check
+          type="radio"
+          label="Brooklyn"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios2"
+        />
+        <Form.Check
+          type="radio"
+          label="Queens"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios3"
+        />
+         <Form.Check
+          type="radio"
+          label="Bronx"
+          name="formHorizontalRadios"
+          id="formHorizontalRadios"
+        />
+      </Col>
     </Form.Group>
+
+
 
     <Form.Group as={Col} controlId="formGridZip">
       <Form.Label>Zip: </Form.Label>
-      <Form.Control />
+      <Form.Control type="number"/>
     </Form.Group>
   </Row>
 
@@ -64,12 +92,13 @@ class Patient extends React.Component {
     <InputGroup.Prepend>
       <InputGroup.Text>Reason for Visit:</InputGroup.Text>
     </InputGroup.Prepend>
-    <FormControl as="textarea" aria-label="With textarea" />
+    <FormControl className="textarea" as="textarea"/> 
   </InputGroup>
+
 
   <Form.Group as={Col} controlId="formGridPhone">
       <Form.Label>Phone: </Form.Label>
-      <Form.Control type="phone" placeholder="Enter phone number" />
+      <Form.Control type="number" placeholder="Enter phone number" />
     </Form.Group>
 
   <Button variant="primary" type="submit">
